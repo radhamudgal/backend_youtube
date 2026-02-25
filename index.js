43bdac59110c5b834194faf4ba021518637cc7c6
radha mudgal <radhamudgal08@gmail.com>
2026-02-25 11:22:15 +0530

    update index.js

--- index.js
+++ index.js
@@ -27,6 +27,9 @@ app.use("/api", userRouter)
 app.use('/api/videos', videoRouter)
 
 app.use("/api/channel", channelRouter)
+app.get("/", (req, res) => {
+    return res.status(200).json({message : "Welcome to youtube backend MRU!!!⚡️ University"})
+})
 
 app.listen(4000, () => {
     console.log("server is running on port: " + 4000)
